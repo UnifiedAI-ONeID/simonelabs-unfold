b767b2e0385268be033b226b98a0b7ffb33175e0
UnifiedAI-ONeID <ONeID_admin@unifiedai.tech>
2025-05-31 00:23:41 -0700

    Set up i18n configuration

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -9,12 +9,10 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    debug: import.meta.env.DEV,
-    
+    debug: false,
     interpolation: {
       escapeValue: false,
     },
-
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
